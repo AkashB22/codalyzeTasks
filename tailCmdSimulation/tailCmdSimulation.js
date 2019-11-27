21e706564ce7b27a48355aea798393278bad957b
akash <akashbalu22@gmail.com>
2019-11-27 14:14:50 +0530

    adding package.json and refactoring code along with comments

--- tailCmdSimulation/tailCmdSimulation.js
+++ tailCmdSimulation/tailCmdSimulation.js
@@ -1,11 +1,38 @@
 let fs = require('fs');
-let data = '';
 let event = require('events');
 let _event = new event();
 let Ws = require('ws').Server;
 let wss = new Ws({host:'localhost', port:5000});
-let lineNos = process.argv[2];
+//let lineNos = process.argv[2];
 
+//Creating a websocket server
+wss.on('connection', (socket)=>{
+    console.log('socket connected');
+    tailCommandSimulator(10, (data)=>{
+        socket.send(data, (err)=>{
+            if(err){
+                console.log('Error on sending msg' + err);
+                return;
+            }
+        })
+    });
+
+    socket.on('message', (lineNos)=>{
+        console.log(lineNos);
+        tailCommandSimulator(lineNos, (data)=>{
+            socket.send(data, (err)=>{
+                if(err){
+                    console.log('Error on sending msg' + err);
+                    return;
+                }
+            })
+        });
+    });
+});
+
+//function that reads data from the test.txt file and returns lastTenLines or lines based on lineNos as a callback 
+//This operation is done on event 'changeFile' which gets trigger manually on 1st function call
+//and later on any changes on that file which is watched for changes using fs.watchFile
 function tailCommandSimulator(lineNos, callback){
     _event.on('changeFile', (fileName)=>{
         fs.readFile(__dirname + '/' + fileName, (err, data)=>{
@@ -38,33 +65,7 @@ function tailCommandSimulator(lineNos, callback){
     _event.emit('changeFile', 'test.txt');
 }
 
-wss.on('connection', (socket)=>{
-    console.log('socket connected');
-    tailCommandSimulator(10, (data)=>{
-        socket.send(data, (err)=>{
-            if(err){
-                //console.log('Error on sending msg' + err);
-                return;
-            }
-        })
-    });
-
-    socket.on('message', (lineNos)=>{
-        console.log(lineNos);
-        tailCommandSimulator(lineNos, (data)=>{
-            socket.send(data, (err)=>{
-                if(err){
-                    //console.log('Error on sending msg' + err);
-                    return;
-                }
-            })
-        });
-    });
-});
-
-
-tailCommandSimulator();
-
+//To server our index.html which has websocket client connection.
 let http = require('http');
 http.createServer((req, res)=>{
     fs.readFile(__dirname + '/index.html', (err, fileData)=>{
@@ -72,4 +73,4 @@ http.createServer((req, res)=>{
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.end(fileData);
     })
-}).listen(3000, () => console.log('server running'));
+}).listen(3000, () => console.log('server running on port 3000 \n open http://localhost:3000 url and change data on test.txt to see the realtime changes'));
